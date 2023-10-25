e5e87da765433764c3c3f04d897abb4e1488630a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-25 10:02:12 -0300

    Estilizando cabeçalho com tailwind @layer.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <header className='bg-slate-700 text-white'>
+    <header className='cabecalho'>
     <nav>
       <ul>
         <li><Link href="/">::.HOME</Link></li>
