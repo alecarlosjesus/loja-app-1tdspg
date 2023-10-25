373b77db97d9fc0aeeeee47f4a57f13bdd00eff6
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-25 09:37:22 -0300

    Criando api local-queijos / Corrigindo bug tailwind postcss

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-    <header className='bg-slate-700 h-20 text-white'>
+    <header className='bg-slate-700 text-white'>
     <nav>
       <ul>
         <li><Link href="/">::.HOME</Link></li>
         <li><Link href="/produtos/camisa">::.Camisas</Link></li>
         <li><Link href="/produtos/tenis">::.Tênis</Link></li>
+        <li><Link href="/fazenda">::.Queijos</Link></li>
       </ul>
     </nav>
   </header>
