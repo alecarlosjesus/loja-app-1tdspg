4a24a07a2f57d9371ca1d59856eadc842a57e350
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-27 16:49:35 -0300

    Consumindo a api-user e validando o POST-LOGIN

--- src/app/api/base/base-users/route.jsx
+++ src/app/api/base/base-users/route.jsx
@@ -1,7 +1,25 @@
 import {promises as fs} from "fs";
 import { NextResponse } from "next/server";
 
+//BASE DE DADOS
+const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
+
 export async function GET(request,{params}) {
-  const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
   return NextResponse.json(JSON.parse(file));
+}
+
+export async function POST(request,response) {
+  const body = await request.json();
+  const usuarios = JSON.parse(file);
+
+  console.log(body.userName);
+  console.log(body.senha);
+
+  for (let x = 0; x < usuarios.usuarios.length; x++) {
+    const u = usuarios.usuarios[x];
+    if(u.userName == body.userName && u.senha == body.senha){
+      return NextResponse.json({ 'status': 'ok' });
+    }
+  }
+      return NextResponse.json({ 'status': 'error' });
 }
