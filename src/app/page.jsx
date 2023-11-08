a9a395a750bbf33cc82c5a95364487a567da642e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-11-08 09:05:59 -0300

    Adicionado o rodape, favicon.ico e estilizando componente de img na HOME.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,17 @@
-import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
+    <div>
       <h1>HOME</h1>
-    </>
+
+        <div className="w-96 m-auto">
+          <figure>
+            <figcaption>Grande Promoção!!!</figcaption>
+            <Image src="/black-friday_640×360.png" alt='Promoção' width={540} height={360}/>
+          </figure>
+        </div>
+
+    </div>
   )
 }
